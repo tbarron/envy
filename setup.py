9aab405808c9a6ec3fe0167661ed81666350cddd
Tom Barron <tom.barron@comcast.net>
2016-12-26 10:45:53 -0500

    - pippability

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='envy',
+      version='0.0.1',
+      description='environment manager and tool',
+      author='Tom Barron',
+      author_email='tusculum@gmail.com',
+      url='https://github.com/tbarron/envy',
+      packages=['envy'],
+      entry_points = {'console_scripts': ['nv = nv:main']}
+      )
